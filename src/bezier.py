69c063a6f7f3679ef40912626203bc223efe1eb1
Bob Dowling <bob.dowling.64@gmail.com>
2025-09-07 13:05:05 +0100

    Basic linear interpolation

---
+++ src/bezier.py
@@ -0,0 +1,21 @@
+import numpy
+
+
+def line(
+    t: numpy.ndarray,
+    x: numpy.ndarray,
+) -> numpy.ndarray:
+    """Linear interpolation between two points, x[0] at t==0 and x[1] at t==1.
+    t:  The interpolation parameter. Expected to hold values between 0.0 and 1.0,
+        but this is not actually required and extrapolation is possible.
+    x:  The end points of the line segment are in x[0] and x[1].
+
+    Returns:    (1.0 - t) * x[0] + t * x[1]
+    """
+    if x.shape[0] != 2:
+        raise ValueError(
+            f"Interpolation function requires exactly two end points in x[0] and x[1]: {x.shape=}"
+        )
+
+    x = (1.0 - t) * x[0] + t * x[1]
+    return x
