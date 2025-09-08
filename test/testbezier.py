8a328a9f99cb255df5851aaa9778fbc68cc8a719
Bob Dowling <bob.dowling.64@gmail.com>
2025-09-08 18:56:29 +0100

    Split testing shape and values

--- test/testbezier.py
+++ test/testbezier.py
@@ -13,7 +13,7 @@ import bezier  # type: ignore
 
 
 class TestLinear(unittest.TestCase):
-    def testPoints(self) -> None:
+    def testPointsShape(self) -> None:
         N = 100
         start = [1.0, 0.0, 0.0]
         stop = [0.0, 1.0, 0.0]
@@ -23,9 +23,20 @@ class TestLinear(unittest.TestCase):
         self.assertEqual(x0.shape, (2, 3))
         x = bezier.line(t, x0)
         self.assertEqual(x.shape, (3, N + 1))
+
+    def testPointsValues(self) -> None:
+        N = 100
+        start = [1.0, 0.0, 0.0]
+        stop = [0.0, 1.0, 0.0]
+        t = numpy.linspace(0.0, 1.0, N + 1, endpoint=True, retstep=False)
+        self.assertEqual(t.shape, (N + 1,))
+        x0 = numpy.array([start, stop])
+        x = bezier.line(t, x0)
         for index in range(3):
             self.assertEqual(x[index, 0], start[index])
             self.assertEqual(x[index, N], stop[index])
+        self.assertAlmostEqual(x[0, N // 2], 0.5)
+        self.assertAlmostEqual(x[1, N // 2], 0.5)
 
 
 if __name__ == "__main__":
